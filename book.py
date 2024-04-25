cb4d4ef90dd6babe5ea5d0d628c19ae79e6db633
Omar Kasmi <Omar.kasmi@protonmail.com>
2024-04-25 16:48:55 +0200

    Add all final files

--- book.py
+++ book.py
@@ -29,9 +29,37 @@ def add_book(request: schemas.book, db: Session = Depends(get_db)):
     return new_book
 
 # Retrieve a list of all books:
+@route.get("/books")
+def get_all_books(db: Session = Depends(get_db)):
+    return db.query(models.book).all()
 
 # Retrieve details for a specific book:
+@route.get("/books/{book_id}")
+def get_book_details(book_id: int, db: Session = Depends(get_db)):
+    return db.query(models.book).filter(models.book.id == book_id).first()
 
 # Update an existing book:
+@route.put("/books/{book_id}")
+def update_book(book_id: int, request: schemas.book, db: Session = Depends(get_db)):
+    book = db.query(models.book).filter(models.book.id == book_id).first()
+    if book:
+        book.title = request.title
+        book.author = request.author
+        book.description = request.description
+        book.published_year = request.published_year
+        book.publisher = request.publisher
+        db.commit()
+        return {"message": "book updated successfully"}
+    else:
+        return {"error": "book not found"}
 
 # delete an existing book:
+@route.delete("/books/{book_id}")
+def delete_book(book_id: int, db: Session = Depends(get_db)):
+    book = db.query(models.book).filter(models.book.id == book_id).first()
+    if book:
+        db.delete(book)
+        db.commit()
+        return {"message": "book deleted successfully"}
+    else:
+        return {"error": "book not found"}
--- user.py
+++ user.py
@@ -10,11 +10,56 @@ route = APIRouter()
 
 
 # Add new user
+@route.post("/users")
+def add_book(request: schemas.User, db: Session = Depends(get_db)):
+    
+    new_user = models.User (name = request.name,
+                           birthday =  request.birthday,
+                           gender = request.gender,
+                           email = request.email
+                        )
+
+    db.add(new_user)
+    db.commit()
+    db.refresh(new_user)
+
+    return new_user
 
 # Retrieve a list of all users:
 
+@route.get("/users")
+def get_users(db: Session = Depends(get_db)):
+    users = db.query(models.User).all()
+    return users
 # Retrieve details for a specific user:
 
+
+
+@route.get("/users/{user_id}", response_model=schemas.User)
+def get_user(user_id: int, db: Session = Depends(get_db)):
+    user = db.query(models.User).filter(models.User.id == user_id).first()
+    if user is None:
+        return "User not found" 
+    return user
+
 # Update an existing user:
+@route.put("/users/{user_id}", response_model=schemas.User)
+def update_user(user_id: int, user_update: schemas.User, db: Session = Depends(get_db)):
+    user = db.query(models.User).filter(models.User.id == user_id).first()
+    if user is None:
+        return "User not found"
+    for var, value in vars(user_update).items():
+        setattr(user, var, value) if value else None
+    db.commit()
+    return user
 
 # delete an existing user:
+
+@route.delete("/users/{user_id}")
+def delete_user(user_id: int, db: Session = Depends(get_db)):
+    user = db.query(models.User).filter(models.User.id == user_id).first()
+    if user is None:
+        return "User not found"
+    db.delete(user)
+    db.commit()
+    return {"message": "User deleted successfully"}
